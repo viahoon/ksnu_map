0987d6a0e4cea44d2d87397cc0ce516006bcb7fa
viahoon <hoon@viasofts.com>
2021-06-24 10:47:16 +0900

    지도타입 변경 스피너 추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
@@ -12,11 +16,15 @@ import com.naver.maps.map.OnMapReadyCallback;
 import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    Spinner maptype_spinner;
+    NaverMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         FragmentManager fm = getSupportFragmentManager();
@@ -27,10 +35,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         mapFragment.getMapAsync(this);
+
+        initViews();
     }
 
     @Override
     public void onMapReady(@NonNull @NotNull NaverMap naverMap) {
-        
+        mMap = naverMap;
+
+        maptype_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    mMap.setMapType(NaverMap.MapType.Basic);
+                } else if (position == 1) {
+                    mMap.setMapType(NaverMap.MapType.Satellite);
+                } else if (position == 2) {
+                    mMap.setMapType(NaverMap.MapType.Terrain);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+    private void initViews() {
+        maptype_spinner = findViewById(R.id.maptype_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.map_type, R.layout.custom_spinner_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_item_click);
+        maptype_spinner.setAdapter(adapter);
+    }
+
 }
